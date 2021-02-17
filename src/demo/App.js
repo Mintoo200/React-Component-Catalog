4b7bd750a5ee1960df024073368109417f079613
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-02-17 16:31:38 +0100

    [DEMO] Added tabs to demo

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import ErrorBanner from '../lib/components/ErrorBanner/ErrorBanner'
+import Tabs from '../lib/components/Tabs/Tabs'
+import Tab from '../lib/components/Tabs/Tab'
 
 const App = () => {
   const [error, setError] = useState(null)
@@ -12,6 +14,14 @@ const App = () => {
   return (
     <div>
       {error && <ErrorBanner message={error} />}
+      <Tabs>
+        <Tab label="This is the first tab">
+          "This is the content of the tab"
+        </Tab>
+        <Tab label="This is the second tab">
+          "This is the content of the second tab"
+        </Tab>
+      </Tabs>
     </div>
   )
 }
