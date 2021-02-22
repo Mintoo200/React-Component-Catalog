55bc7c80b2dbbd62fb15be2e66c303106987dec9
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-02-22 15:49:45 +0100

    [TEST] Added error on console.error

--- testsSetup.ts
+++ testsSetup.ts
@@ -3,3 +3,10 @@ import Adapter from 'enzyme-adapter-react-16'
 import 'jest-enzyme'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+/* eslint-disable */
+const previousConsole = console.error
+console.error = (message) => {
+  previousConsole(message)
+  throw new Error(message)
+}
