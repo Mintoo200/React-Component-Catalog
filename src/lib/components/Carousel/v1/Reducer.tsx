68ce63976ce5f2ece018901966c3dac4914d0bf8
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-11 11:37:17 +0100

    [CAROUSEL] added basic reducer

---
+++ src/lib/components/Carousel/v1/Reducer.tsx
@@ -0,0 +1,52 @@
+import { Reducer as ReducerType } from 'react'
+import InvalidActionError from '../../Errors/InvalidActionError'
+import { ContextType } from './Context'
+
+export type Action = {
+  type: ReducerActions,
+  slideCount?: number,
+  slideIndex?: number,
+}
+
+export enum ReducerActions {
+  nextSlide,
+  previousSlide,
+  setCurrentSlide,
+  setSlideCount,
+}
+
+export const Reducer: ReducerType<ContextType, Action> = (state, action) => {
+  switch (action.type) {
+    case ReducerActions.nextSlide:
+      return {
+        ...state,
+        currentSlide: (state.currentSlide + 1) % state.slideCount,
+      }
+
+    case ReducerActions.previousSlide:
+      return {
+        ...state,
+        currentSlide: (state.currentSlide - 1 + state.slideCount) % state.slideCount,
+      }
+
+    case ReducerActions.setCurrentSlide:
+      return {
+        ...state,
+        currentSlide: action.slideIndex % state.slideCount,
+      }
+
+    case ReducerActions.setSlideCount:
+      return {
+        ...state,
+        slideCount: action.slideCount,
+        currentSlide: (state.currentSlide < action.slideCount)
+          ? state.currentSlide
+          : action.slideCount - 1,
+      }
+
+    default:
+      throw new Error(`Invalid action with ID ${action.type}`)
+  }
+}
+
+export default Reducer
