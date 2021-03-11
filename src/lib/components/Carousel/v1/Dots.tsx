59d324d0c76b6c8f3f6a8ff12118339ce782c0f8
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-11 12:37:48 +0100

    [CAROUSEL] Added dots controls

---
+++ src/lib/components/Carousel/v1/Dots.tsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import Context from './Context'
+import { ReducerActions } from './Reducer'
+
+const Dots: React.FC = () => {
+  const { currentSlide, slideCount, dispatch } = useContext(Context)
+  return (
+    <ul className="dots">
+      {[...Array(slideCount)].map((_, index) => (
+        <li>
+          <button
+            className="dot-button"
+            type="button"
+            onClick={() => dispatch({
+              type: ReducerActions.setCurrentSlide,
+              slideIndex: index,
+            })}>
+            <div className={`dot ${currentSlide === index ? 'dot-active' : ''}`} />
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default Dots
