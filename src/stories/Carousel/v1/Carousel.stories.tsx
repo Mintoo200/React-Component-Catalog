626689d92db9b09bd2b63014914d3e96358b57fb
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-11 13:07:00 +0100

    [STORIES] Added story for custom buttons on Carousel

--- src/stories/Carousel/v1/Carousel.stories.tsx
+++ src/stories/Carousel/v1/Carousel.stories.tsx
@@ -88,3 +88,25 @@ dotsInControls.args = {
     </Controls>,
   ],
 }
+
+export const customButtons = Template.bind({})
+customButtons.args = {
+  children: [
+    <Slides>
+      <Slide><img src="https://picsum.photos/id/237/200/200" alt="A good boi" /></Slide>
+      <Slide><img src="https://picsum.photos/id/1/200/200" alt="me rn" /></Slide>
+      <Slide><img src="https://picsum.photos/id/1002/200/200" alt="Now I'm thursty" /></Slide>
+      <Slide><img src="https://picsum.photos/id/1003/200/200" alt="Oh deer!" /></Slide>
+      <Slide><img src="https://picsum.photos/id/1015/200/200" alt="The amazon or something" /></Slide>
+    </Slides>,
+    <Dots />,
+    <Controls>
+      <Previous>Hello</Previous>
+      <Play>
+        <div>Start</div>
+        <div>Stop</div>
+      </Play>
+      <Next>Bye</Next>
+    </Controls>,
+  ],
+}
