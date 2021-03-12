703fe7878f149cecad4b3108d8ba9c03d760f8f4
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-12 18:11:26 +0100

    [STORIES] Added stories for multiple APIs

--- src/stories/useAPI/useAPI.stories.tsx
+++ src/stories/useAPI/useAPI.stories.tsx
@@ -1,9 +1,11 @@
-/* eslint-disable */
 import React, { useEffect, useState } from 'react'
 import { Story } from '@storybook/react'
-import useAPI, { APIContext, Props as APIContextProps } from '../../lib/hooks/useAPI/useAPI'
-import APIClass, { APIClassConstructor } from '../../lib/hooks/useAPI/APIClass'
 import { AxiosResponse } from 'axios'
+import useAPI from '../../lib/hooks/useAPI/useAPI'
+import APIContext, { Props as APIContextProps } from '../../lib/hooks/useAPI/Context'
+import { APIClassConstructor } from '../../lib/hooks/useAPI/APIClass'
+import MyAPI from './MyAPI'
+import MySecondAPI from './MySecondAPI'
 
 export default {
   title: 'useAPI/useAPI',
@@ -15,47 +17,35 @@ type CancelablePromise<ReturnType> = {
 }
 
 function makeCancelable<ReturnType>(promise: Promise<ReturnType>): CancelablePromise<ReturnType> {
-  let hasCanceled_ = false;
+  let hasCanceled = false
 
   const wrappedPromise = new Promise<ReturnType>((resolve, reject) => {
     promise.then(
-      val => hasCanceled_ ? reject({isCanceled: true}) : resolve(val),
-      error => hasCanceled_ ? reject({isCanceled: true}) : reject(error)
-    );
-  });
+      (val) => ((hasCanceled) ? reject(new Error('Canceled')) : resolve(val)),
+      (error) => ((hasCanceled) ? reject(new Error('Canceled')) : reject(error)),
+    )
+  })
 
   return {
     promise: wrappedPromise,
     cancel() {
-      hasCanceled_ = true;
+      hasCanceled = true
     },
-  };
-};
-
-class myAPI extends APIClass {
-  async getFile(): Promise<AxiosResponse> {
-    return this.axios.get('/projects/24477877/repository/files/src%2Flib%2Fcomponents%2FTabs%2Fv1%2FTabs%2Etsx/raw?ref=master')
-  }
-
-  async getFileWithTimeout(timer = 1000): Promise<AxiosResponse> {
-    const response = await this.getFile()
-    await setTimeout(() => null, timer)
-    return response
   }
 }
 
-const Component: React.FC<{timeout?: boolean}> = ({ timeout = false }) => {
+const SingleAPIComponent: React.FC<{timeout?: boolean}> = ({ timeout = false }) => {
   const [file, setFile] = useState(null as string)
-  const API = useAPI<myAPI>()
+  const API = useAPI<MyAPI>()
   useEffect(() => {
     let promise = null as CancelablePromise<AxiosResponse>
     if (timeout) {
       promise = makeCancelable<AxiosResponse>(API.getFileWithTimeout())
-      promise.promise.then((response) => setFile(response.data))
     } else {
       promise = makeCancelable<AxiosResponse>(API.getFile())
-      promise.promise.then((response) => setFile(response.data))
     }
+    promise.promise.then((response) => setFile(response.data))
+      .catch(() => null)
     return () => promise.cancel()
   }, [])
   return (
@@ -65,21 +55,22 @@ const Component: React.FC<{timeout?: boolean}> = ({ timeout = false }) => {
   )
 }
 
-const Template: Story<APIContextProps<APIClassConstructor<myAPI>>> = (args) => (
-  <APIContext {...args}>
-    <Component />
-  </APIContext>
+const Template: Story<APIContextProps<APIClassConstructor<MyAPI>>> = (args) => (
+  <APIContext {...args} />
 )
 
 export const Default = Template.bind({})
 Default.args = {
   url: 'https://gitlab.com/api/v4',
-  APIClass: myAPI,
+  APIClass: MyAPI,
+  children: [
+    <SingleAPIComponent />,
+  ],
 }
 
-const Template2: Story<APIContextProps<APIClassConstructor<myAPI>>> = (args) => {
+const Template2: Story<APIContextProps<APIClassConstructor<MyAPI>>> = (args) => {
   const [content, setContent] = useState(
-    <Component timeout />,
+    <SingleAPIComponent timeout />,
   )
   useEffect(() => {
     setTimeout(() => {
@@ -97,5 +88,57 @@ const Template2: Story<APIContextProps<APIClassConstructor<myAPI>>> = (args) => 
 export const Unmount = Template2.bind({})
 Unmount.args = {
   url: 'https://gitlab.com/api/v4',
-  APIClass: myAPI,
+  APIClass: MyAPI,
+}
+
+const MultiAPIComponent1: React.FC = () => {
+  const [file, setFile] = useState(null as string)
+  const API = useAPI<MyAPI>('gitlab')
+  useEffect(() => {
+    const promise = makeCancelable<AxiosResponse>(API.getFile())
+    promise.promise.then((response) => setFile(response.data))
+    promise.promise.catch()
+    return () => promise.cancel()
+  }, [])
+  return (
+    <div style={{ whiteSpace: 'pre' }}>
+      {(file != null) ? file : 'waiting...'}
+    </div>
+  )
+}
+
+const MultiAPIComponent2: React.FC = () => {
+  const [file, setFile] = useState(null as string)
+  const API = useAPI<MySecondAPI>('also gitlab')
+  useEffect(() => {
+    const promise = makeCancelable<AxiosResponse>(API.getFile())
+    promise.promise.then((response) => setFile(response.data))
+    promise.promise.catch()
+    return () => promise.cancel()
+  }, [])
+  return (
+    <div style={{ whiteSpace: 'pre' }}>
+      {(file != null) ? file : 'waiting...'}
+    </div>
+  )
+}
+
+export const MultipleAPI = Template.bind({})
+MultipleAPI.args = {
+  APIs: [
+    {
+      name: 'gitlab',
+      url: 'https://gitlab.com/api/v4',
+      APIClass: MyAPI,
+    },
+    {
+      name: 'also gitlab',
+      url: 'https://gitlab.com/api/v4',
+      APIClass: MySecondAPI,
+    },
+  ],
+  children: [
+    <MultiAPIComponent1 />,
+    <MultiAPIComponent2 />,
+  ],
 }
