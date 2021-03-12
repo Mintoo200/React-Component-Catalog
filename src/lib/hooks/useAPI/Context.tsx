9c2fbd8fe10fd168ffb81b8f568e33a1b7ab9605
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-12 18:10:37 +0100

    [HOOKS] Added support for multiple APIs

---
+++ src/lib/hooks/useAPI/Context.tsx
@@ -0,0 +1,73 @@
+import axios from 'axios'
+import React from 'react'
+import BaseAPIClass, { BaseAPIClassConstructor } from './APIClass'
+
+export const Context = React.createContext({} as Record<string, BaseAPIClass>)
+
+export type SingleAPIProps<T extends BaseAPIClassConstructor> = {
+  url: string,
+  APIClass: T
+  children: React.ReactNode,
+}
+
+export type MultiAPIProps = {
+  APIs: {
+    name: string,
+    url: string,
+    APIClass: BaseAPIClassConstructor,
+  }[],
+  children: React.ReactNode,
+}
+
+function isMultiAPI<T extends BaseAPIClassConstructor>(props: Props<T>): props is MultiAPIProps<T> {
+  return (props as MultiAPIProps).APIs != null
+}
+
+export type Props<T extends BaseAPIClassConstructor> = SingleAPIProps<T>|MultiAPIProps<T>
+
+function MultiAPIContext(
+  { APIs, children }: MultiAPIProps,
+): React.ReactElement {
+  const APIInstances = {} as Record<string, BaseAPIClass>
+  APIs.forEach(({ name, url, APIClass }) => {
+    const axiosInstance = axios.create({
+      baseURL: url,
+    })
+    const APIInstance = new APIClass(axiosInstance)
+    APIInstances[name] = APIInstance
+  })
+  return (
+    <Context.Provider value={APIInstances}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+function SingleAPIContext<T extends BaseAPIClassConstructor>(
+  { url, APIClass, children }: SingleAPIProps<T>,
+): React.ReactElement {
+  return (
+    <MultiAPIContext APIs={[
+      {
+        name: 'default',
+        url,
+        APIClass,
+      },
+    ]}>
+      {children}
+    </MultiAPIContext>
+  )
+}
+
+export function APIContext<T extends BaseAPIClassConstructor>(
+  props: Props<T>,
+): React.ReactElement {
+  if (isMultiAPI(props)) {
+    return <MultiAPIContext {...props} />
+  }
+  return (
+    <SingleAPIContext {...props} />
+  )
+}
+
+export default APIContext
