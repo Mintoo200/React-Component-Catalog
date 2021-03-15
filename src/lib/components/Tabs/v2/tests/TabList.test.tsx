11f746d1e963518aebf2432723cef712c7e7ae59
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-15 13:16:25 +0100

    [ERRORS] Changed error file structure

--- src/lib/components/Tabs/v2/tests/TabList.test.tsx
+++ src/lib/components/Tabs/v2/tests/TabList.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import TabList from '../TabList'
 import Tab from '../Tab'
-import NoContextError from '../../../Errors/NoContextError'
+import NoContextError from '../../../../errors/NoContextError'
 
 describe('Tab tests', (): void => {
   it('should throw when click on tab with no context', () => {
