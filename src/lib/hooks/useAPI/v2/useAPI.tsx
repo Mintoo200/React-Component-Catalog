cbac5a044a255374be8921a787f5ae67f5ec8e80
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-15 15:41:14 +0100

    [HOOKS] Added v2 for useAPI

---
+++ src/lib/hooks/useAPI/v2/useAPI.tsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react'
+import BaseAPIClass from '../APIClass'
+import { Context } from './Context'
+
+// FIXME: The generic type T does not exist at runtime,
+// it is therefore impossible to ensure that APIInstance
+// actually extends T. I can return an instance of T
+// but not ensure that it actually is an instance of it.
+// If you get a 'is not a function' error, you probably
+// don't have the right class stored in the context
+function useAPI<T extends BaseAPIClass>(name:string = null): T {
+  const { APIs } = useContext(Context)
+  return APIs[(name != null) ? name : 'default'] as T
+}
+
+export default useAPI
