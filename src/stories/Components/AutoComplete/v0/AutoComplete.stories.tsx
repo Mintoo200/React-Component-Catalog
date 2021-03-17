4aca40bd08673a34894dbbf8c9fa9e65146344b1
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-17 17:33:18 +0100

    [STORIES] Added story for autocomplete v0

---
+++ src/stories/Components/AutoComplete/v0/AutoComplete.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import AutoComplete, { Props as AutoCompleteProps } from '../../../../lib/components/AutoComplete/v0/AutoComplete'
+
+export default {
+  title: 'Components/AutoComplete/v0 - Option list',
+  component: AutoComplete,
+}
+
+const Template: Story<AutoCompleteProps> = (args) => (
+  <AutoComplete {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  options: [
+    { value: 'My First Value', label: 'This is the label for the first value' },
+    { value: 'My Second Value', label: 'Also with a label' },
+    { value: 'My Third Value' },
+    { value: 'My Fourth Value', label: 'Notice how the third value had no label' },
+  ],
+}
