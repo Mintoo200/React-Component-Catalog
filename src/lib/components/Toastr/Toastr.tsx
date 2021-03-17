e6670455b0f7157ab98facd711f250569426fa9f
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-17 13:06:54 +0100

    [TOASTR] Added timer to toastr

--- src/lib/components/Toastr/Toastr.tsx
+++ src/lib/components/Toastr/Toastr.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import './style.css'
 
 export type Props = {
   children: React.ReactNode,
   isOpen?: boolean,
+  onClose?: () => void,
+  timer?: number,
 }
 
 const Toastr: React.FC<Props> = ({
-  children, isOpen = false,
-}) => (
-  <div className={`toastr ${isOpen ? '' : 'hidden'}`}>
-    {children}
-  </div>
-)
+  children, isOpen = false, onClose = null, timer = null,
+}) => {
+  useEffect(() => {
+    if (timer != null) {
+      if (onClose == null) {
+        throw new Error('onClose is required with timer.')
+      }
+      const timeout = setTimeout(onClose, timer)
+      return () => clearTimeout(timeout)
+    }
+    return null
+  }, [isOpen])
+  return (
+    <div className={`toastr ${isOpen ? '' : 'hidden'}`}>
+      {children}
+    </div>
+  )
+}
 
 export default Toastr
