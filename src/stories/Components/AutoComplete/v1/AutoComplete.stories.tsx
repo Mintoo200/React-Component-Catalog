509c52ba5e90826196ab68e8d2606c496306ac18
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-22 17:09:27 +0100

    [STORIES] Added doc to AutoComplete v1

--- src/stories/Components/AutoComplete/v1/AutoComplete.stories.tsx
+++ src/stories/Components/AutoComplete/v1/AutoComplete.stories.tsx
@@ -2,9 +2,35 @@ import React from 'react'
 import { Story } from '@storybook/react'
 import AutoComplete, { Props as AutoCompleteProps } from '../../../../lib/components/AutoComplete/v1/AutoComplete'
 
+const documentation = `
+## API
+\`\`\`tsx
+<AutoComplete
+  getOptions={async () => [
+    { value: 'My First Value', label: 'This is the label for the first value' },
+    { value: 'My Second Value', label: 'Also with a label' },
+    { value: 'My Third Value' },
+    { value: 'My Fourth Value', label: 'Notice how the third value had no label' },
+  ]}
+  onSubmit={(input: string) => null} />
+\`\`\`
+learn more [here](/story/components-autocomplete-study--page#version-1--options-as-function)
+`
+
 export default {
   title: 'Components/AutoComplete/v1 - Options as function',
   component: AutoComplete,
+  parameters: {
+    componentSource: {
+      url: 'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fcomponents%2FAutoComplete%2Fv1%2FAutoComplete%2Etsx/raw?ref=master',
+      language: 'javascript',
+    },
+    docs: {
+      description: {
+        component: documentation,
+      },
+    },
+  },
 }
 
 const Template: Story<AutoCompleteProps> = (args) => (
