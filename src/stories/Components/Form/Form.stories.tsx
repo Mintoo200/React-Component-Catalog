8b5435b2b9c4a2e932c5e48e3490ba8797866fd8
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-23 17:08:06 +0100

    [STORIES] Added basic stories for Form component

---
+++ src/stories/Components/Form/Form.stories.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { Story } from '@storybook/react'
+import Form, { FormState, Props as FormProps } from '../../../lib/components/Form/Form'
+
+export default {
+  title: 'Components/Form/Form',
+  component: Form,
+}
+
+const Template: Story<FormProps> = () => {
+  const [form, setForm] = useState(null as FormState)
+  return (
+    <Form onChange={setForm}>
+      <input id="id-1" type="email" required minLength={10} />
+      { form && !form.content['id-1'].validity.valid && <span>Email is not valid!</span> }
+      { form && form.content['id-1'].validity.valid && <span>Yay! Nice email</span> }
+      <button type="submit" disabled={!form || !form.valid}>Submit</button>
+    </Form>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {}
+
+const GenericTemplate: Story<FormProps> = (args) => (
+  <Form {...args} />
+)
+
+export const nestedFields = GenericTemplate.bind({})
+nestedFields.args = {
+  children: [
+    <input id="input" />,
+    <div style={{ background: 'cyan', padding: '5px' }}>
+      <input id="input-nested" required />
+      <div style={{ background: 'pink', padding: '5px' }}>
+        <input id="input-nested-2" required />
+      </div>
+    </div>,
+  ],
+}
+
+export const defaultValues = GenericTemplate.bind({})
+defaultValues.args = {
+  children: [
+    <input id="input" defaultValue="Default value" />,
+  ],
+}
+
+export const otherFieldTypes = GenericTemplate.bind({})
+otherFieldTypes.args = {
+  children: [
+    <input id="input" placeholder="<input />" />,
+    <textarea id="textarea" placeholder="<textarea />" />,
+    <select id="select" placeholder="<select />">
+      <option>Maybe</option>
+      <option>Blouge</option>
+      <option>200</option>
+      <option>Also</option>
+    </select>,
+  ],
+}
