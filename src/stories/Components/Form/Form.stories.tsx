24393a2061f531982206001a48d24c1e59ad0a87
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-25 12:24:53 +0100

    [STORIES] Added doc to Form

--- src/stories/Components/Form/Form.stories.tsx
+++ src/stories/Components/Form/Form.stories.tsx
@@ -2,9 +2,64 @@ import React, { useState } from 'react'
 import { Story } from '@storybook/react'
 import Form, { FormState, Props as FormProps } from '../../../lib/components/Form/Form'
 
+const documentation = `
+## API
+\`\`\`tsx
+<Form
+  onChange={(formState: FormState) => null}
+  onSubmit={(formState: FormState) => null}>
+  <input id="my-input" />
+  <button type="submit">Submit</button>
+</Form>
+
+type FormState = {
+  valid: boolean,
+  content: Record<string, FieldValue<unknown>>,
+}
+
+type FieldValue<T> = {
+  validity: ValidityState,
+  pristine: boolean,
+  value: T,
+}
+
+// ValidityState = {
+//   badInput: boolean,
+//   customError: boolean,
+//   patternMismatch: boolean,
+//   rangeOverflow: boolean,
+//   rangeUnderflow: boolean,
+//   stepMismatch: boolean,
+//   tooLong: boolean,
+//   tooShort: boolean,
+//   typeMismatch: boolean,
+//   valid: boolean,
+//   valueMissing: boolean,
+// }
+\`\`\`
+\`onChange\` is called after each edit in the form, **including when an input looses its pristine state**.  
+**It is also called after the first render**, before any edit, with the default value for each field.  
+\`onSubmit\` is called on click on any component with \`type="submit"\`  
+\`ValidityState\` [reference](https://developer.mozilla.org/en-US/docs/Web/API/ValidityState).
+`
+
 export default {
   title: 'Components/Form/Form',
   component: Form,
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+    componentSource: {
+      url: [
+        'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fcomponents%2FForm%2FForm%2Etsx/raw?ref=master',
+      ],
+      language: 'javascript',
+    },
+    docs: {
+      description: {
+        component: documentation,
+      },
+    },
+  },
 }
 
 const Template: Story<FormProps> = () => {
