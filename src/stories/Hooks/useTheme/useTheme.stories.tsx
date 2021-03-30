62acb64acd0d78a2b91af3fa091d51496d16bce6
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-03-30 16:21:03 +0200

    [STORIES] Added doc to useTheme

--- src/stories/Hooks/useTheme/useTheme.stories.tsx
+++ src/stories/Hooks/useTheme/useTheme.stories.tsx
@@ -4,8 +4,60 @@ import useTheme, { ThemeProvider, Props as ThemeProviderProps, Themes } from '..
 
 import './style.css'
 
+const documentation = `
+## API
+
+\`\`\`tsx
+<ThemeProvider>
+  <MyComponent />
+</ThemeProvider>
+
+const MyComponent = () => {
+  const {theme, setTheme} = useTheme()
+  return (
+    ...
+  )
+}
+\`\`\`
+\`\`\`css
+body[data-theme="light"] {
+  --primary: black;
+  --secondary: white;
+}
+
+body[data-theme="dark"] {
+  --primary: white;
+  --secondary: black;
+}
+
+.theme-button {
+  color: var(--primary);
+  background: var(--secondary);
+}
+\`\`\`
+
+## Optionnal props
+
+\`defaultTheme\`: defaults to \`Themes.light\`
+`
+
 export default {
   title: 'Hooks/useTheme/useTheme',
+  component: ThemeProvider,
+  parameters: {
+    componentSource: {
+      url: [
+        'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fhooks%2FuseTheme%2FuseTheme%2Etsx/raw?ref=master',
+        'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fhooks%2FuseTheme%2FThemes%2Etsx/raw?ref=master',
+      ],
+      language: 'javascript',
+    },
+    docs: {
+      description: {
+        component: documentation,
+      },
+    },
+  },
 }
 
 const ThemeButton = () => {
@@ -26,6 +78,7 @@ const Template: Story<ThemeProviderProps> = (args) => (
 
 export const Default = Template.bind({})
 Default.args = {
+  defaultTheme: Themes.dark,
   children: [
     <ThemeButton />,
   ],
