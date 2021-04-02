7f4ebf0046043c8c481232cf73ba2f2250ca9cae
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-04-02 11:06:05 +0200

    [STORIES] Added CSS to Overlay stories

--- src/stories/Components/Overlay/Overlay.stories.tsx
+++ src/stories/Components/Overlay/Overlay.stories.tsx
@@ -35,6 +35,7 @@ export default {
     componentSource: {
       url: [
         'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fcomponents%2FOverlay%2FOverlay%2Etsx/raw?ref=master',
+        'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fcomponents%2FOverlay%2Fstyle%2Ecss/raw?ref=master',
       ],
       language: 'javascript',
     },
