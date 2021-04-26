96d9a4aa2c37e6a998a173eb025bc455b905db18
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-04-26 14:30:52 +0200

    [STORIES] Added doc to useCombinedRef hook

--- src/stories/Hooks/useCombinedRef/useCombinedRef.stories.tsx
+++ src/stories/Hooks/useCombinedRef/useCombinedRef.stories.tsx
@@ -2,6 +2,26 @@ import React, { useState } from 'react'
 import { Story } from '@storybook/react'
 import useCombinedRef from '../../../lib/hooks/useCombinedRef/useCombinedRef'
 
+const documentation = `
+This hook synchronizes any number of ref and returns a new RefObject.  
+It will call any ref callback and update any RefObject given with the returned ref.  
+
+## API
+
+\`\`\`tsx
+const MyComponent = React.forwardRef<HTMLButtonElement, Props>(
+  (props, forwardedRef) => {
+    const ref = useCombinedRef(forwardedRef)
+    return (
+      <button ref={ref} />
+    )
+  }
+)
+\`\`\`
+
+This hook takes a variadic ref array, allowing for any number of ref given as argument as follow: \`useCombinedRef(ref1, ref2, ref3, ...)\`
+`
+
 export default {
   title: 'Hooks/useCombinedRef/useCombinedRef',
   parameters: {
@@ -10,6 +30,11 @@ export default {
         'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fhooks%2FuseCombinedRef%2FuseCombinedRef%2Etsx/raw?ref=master',
       ],
       language: 'javascript',
+    },
+    docs: {
+      description: {
+        component: documentation,
+      },
     },
   },
 }
