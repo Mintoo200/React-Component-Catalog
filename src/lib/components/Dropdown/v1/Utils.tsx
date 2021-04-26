979bd989a6db5acc86711baa0e26dd03e5ac19f4
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-04-26 16:21:27 +0200

    [DROPDOWN] Added util function for matching character keys

---
+++ src/lib/components/Dropdown/v1/Utils.tsx
@@ -0,0 +1,43 @@
+export function isCharacter(key: string): boolean {
+  return key != null && key.length === 1
+}
+
+export function matchFirstCharacter(text: string, key: string): boolean {
+  return text != null
+    && key != null
+    && text.substring(0, 1).toLowerCase() === key.toLowerCase()
+}
+
+export function shiftArray<T>(array: T[], from: number): T[] {
+  const newArray = array.slice(from).concat(array.slice(0, from))
+  return newArray
+}
+
+interface TextContent {
+  current: {
+    textContent?: string
+  }
+}
+
+export function findFirstMatchingElement(
+  array: TextContent[],
+  match: string,
+): number {
+  const predicate = (item: TextContent) => (
+    matchFirstCharacter(item?.current?.textContent, match)
+  )
+  return array.findIndex(predicate)
+}
+
+export function findNextMatching(
+  array: TextContent[],
+  match: string,
+  from: number,
+): number {
+  const shiftedArray = shiftArray(array, from + 1)
+  const newShiftedIndex = findFirstMatchingElement(shiftedArray, match)
+  if (newShiftedIndex === -1) {
+    return newShiftedIndex
+  }
+  return (newShiftedIndex + from + 1) % array.length
+}
