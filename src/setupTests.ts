2314bfb3fda959805b228539445a37ef93336ecb
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-04-27 14:06:28 +0200

    [CONFIG] Removed enzyme from dependencies

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,4 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import 'jest-enzyme'
 import '@testing-library/jest-dom/extend-expect'
-
-Enzyme.configure({ adapter: new Adapter() })
 
 /* eslint-disable no-console */
 const previousConsole = console.error
