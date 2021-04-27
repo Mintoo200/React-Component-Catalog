ce04361d46cf1198293528f9d699f23117f83968
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-04-27 14:03:37 +0200

    [TESTS] Migrated Tabs v3 to testing-library

--- src/lib/components/Tabs/v3/TabContent.tsx
+++ src/lib/components/Tabs/v3/TabContent.tsx
@@ -8,23 +8,22 @@ type Props = {
 const TabContent = ({ children }: Props): React.ReactElement => {
   const { activeIndex } = useTabs()
   /* Renders only the current tab */
+  // return (
+  //   <div className="content">
+  //     {children[activeIndex]}
+  //   </div>
+  // )
+  /* Renders hidden tabs with `display: none;` */
   return (
     <div className="content">
-      {children[activeIndex]}
+      {React.Children.map(children, (child, index) => (
+        <div
+          key={index}
+          className={`content ${activeIndex === index ? '' : 'hidden'}`}>
+          {child}
+        </div>
+      ))}
     </div>
   )
-  /* Renders hidden tabs with `display: none;`
-  * return (
-  *   <div className="content">
-  *     {React.Children.map(children, (child, index) => (
-  *       <div
-  *         key={index}
-  *         className={`content ${activeIndex === index ? '' : 'hidden'}`}>
-  *         {child}
-  *       </div>
-  *     ))}
-  *   </div>
-  * )
-  */
 }
 export default TabContent
--- src/lib/components/Tabs/v3/tests/Tabs.test.tsx
+++ src/lib/components/Tabs/v3/tests/Tabs.test.tsx
@@ -1,24 +1,67 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 import Tabs from '../Tabs'
 import Tab from '../Tab'
 import TabList from '../TabList'
 import TabContent from '../TabContent'
 
+import '../../style.css'
+
 describe('Tabs tests', (): void => {
-  it('should render a context', () => {
-    const wrapper = shallow(
-      <Tabs>
-        <TabList>
-          <Tab>Tab1</Tab>
-          <Tab>Tab2</Tab>
-        </TabList>
-        <TabContent>
-          <div>Content1</div>
-          <div>Content2</div>
-        </TabContent>
-      </Tabs>,
+  it('should render the content of the active tab', (): void => {
+    render(
+      <>
+        <style>{'.hidden { display: none; }'}</style>
+        <Tabs>
+          <TabList>
+            <Tab>Tab1</Tab>
+            <Tab>Tab2</Tab>
+          </TabList>
+          <TabContent>
+            <div>Content1</div>
+            <div>Content2</div>
+          </TabContent>
+        </Tabs>
+      </>,
     )
-    expect(wrapper).toContainMatchingElement('ContextProvider')
+    expect(screen.getByText('Content1')).toBeVisible()
+  })
+  it('should render hidden tabs as hidden', (): void => {
+    render(
+      <>
+        <style>{'.hidden { display: none; }'}</style>
+        <Tabs>
+          <TabList>
+            <Tab>Tab1</Tab>
+            <Tab>Tab2</Tab>
+          </TabList>
+          <TabContent>
+            <div>Content1</div>
+            <div>Content2</div>
+          </TabContent>
+        </Tabs>
+      </>,
+    )
+    expect(screen.getByText('Content2')).not.toBeVisible()
+  })
+  it('should switch shown tab on click', (): void => {
+    render(
+      <>
+        <style>{'.hidden { display: none; }'}</style>
+        <Tabs>
+          <TabList>
+            <Tab>Tab1</Tab>
+            <Tab>Tab2</Tab>
+          </TabList>
+          <TabContent>
+            <div>Content1</div>
+            <div>Content2</div>
+          </TabContent>
+        </Tabs>
+      </>,
+    )
+    screen.getByText('Tab2').click()
+    expect(screen.getByText('Content1')).not.toBeVisible()
+    expect(screen.getByText('Content2')).toBeVisible()
   })
 })
