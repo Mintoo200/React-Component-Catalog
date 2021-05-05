2c86ada0159ff8e88ca37da01d0cd5d8c3b74513
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-05-05 18:26:52 +0200

    [STORIES] Added doc to useSyncFocus

--- src/stories/Hooks/useFocusSync/useFocusSync.stories.tsx
+++ src/stories/Hooks/useFocusSync/useFocusSync.stories.tsx
@@ -2,6 +2,97 @@ import React, { useRef } from 'react'
 import { Story } from '@storybook/react'
 import useFocusSync, { SyncProvider } from '../../../lib/hooks/useFocusSync/useFocusSync'
 
+const documentation = `
+## API
+
+\`\`\`tsx
+type SyncObject = {
+  hasFocus: boolean,
+  hasDirectFocus: boolean,
+  hasIndirectFocus: boolean,
+  syncedChildHasFocus: boolean,
+  syncFocus: (event: React.FocusEvent<HTMLElement>) => void,
+}
+
+const MyComponent = () => {
+  const ref = useRef<HTMLButtonElement>()
+  const sync: SyncObject = useSyncFocus(ref)
+  return (
+    <button ref={ref} onFocus={sync.syncFocus} onBlur={sync.syncFocus}>
+      Click Me!
+    </button>
+  )
+}
+
+const App = () => (
+  <SyncProvider>
+    <MyComponent />
+  </SyncProvider>
+)
+\`\`\`
+
+\`syncFocus\` is a callback that syncs the focus between all synced component. It needs to be called on each synced component on focus and blur:
+
+\`\`\`tsx
+<button ref={ref} onFocus={syncFocus} onBlur={syncFocus} />
+\`\`\`
+
+\`hasFocus\` is true if the given ref **or any of its children** is focussed:
+
+\`\`\`tsx
+<div synced focussed>     {/* <-- hasFocus is true */}
+  <div />
+</div>
+
+<div synced>              {/* <-- hasFocus is true */}
+  <div focussed />
+</div>
+\`\`\`
+
+\`hasDirectFocus\` is true if the given ref is directly focussed:
+
+\`\`\`tsx
+<div synced focussed>     {/* <-- hasDirectFocus is true */}
+  <div />
+</div>
+
+<div synced>              {/* <-- hasDirectFocus is false */}
+  <div focussed />
+</div>
+\`\`\`
+
+\`hasIndirectFocus\` is true if the given ref is strictly indirectly focussed:
+
+\`\`\`tsx
+<div synced focussed>     {/* <-- hasIndirectFocus is false */}
+  <div />
+</div>
+
+<div synced>              {/* <-- hasIndirectFocus is true */}
+  <div focussed />
+</div>
+\`\`\`
+
+\`syncedChildHasFocus\` is true if the given ref is strictly indirectly focussed and a synced child of this ref has focus **and only if the synced child uses the same SyncProvider** (cf. [this story](#synced-child-with-multiple-providers)):
+
+\`\`\`tsx
+<div synced focussed>     {/* <-- syncedChildHasFocus is false (focus is direct) */}
+  <div />
+  <div synced />
+</div>
+
+<div synced>              {/* <-- syncedChildHasFocus is false (focus is not on a synced child) */}
+  <div focussed />
+  <div synced />
+</div>
+
+<div synced>              {/* <-- syncedChildHasFocus is true */}
+  <div />
+  <div synced focussed />
+</div>
+\`\`\`
+`
+
 export default {
   title: 'Hooks/useFocusSync/useFocusSync',
   parameters: {
@@ -10,6 +101,11 @@ export default {
         'https://gitlab.com/api/v4/projects/24477877/repository/files/src%2Flib%2Fhooks%2FuseSyncFocus%2FuseSyncFocus%2Etsx/raw?ref=master',
       ],
       language: 'javascript',
+    },
+    docs: {
+      description: {
+        component: documentation,
+      },
     },
   },
 }
