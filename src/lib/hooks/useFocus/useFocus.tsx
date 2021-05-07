943e0260f8f782de7eb3c048a7a2a869bf0290be
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-05-07 14:30:30 +0200

    [HOOKS] Changed effect of useFocus to layout effect

--- src/lib/hooks/useFocus/useFocus.tsx
+++ src/lib/hooks/useFocus/useFocus.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 
 export default function useFocus<T extends HTMLElement>(
   hasFocus: boolean,
   ref: React.MutableRefObject<T> = useRef<T>(),
 )
 : React.MutableRefObject<T> {
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (hasFocus) {
       ref?.current?.focus()
     }
