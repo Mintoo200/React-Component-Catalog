bd98d16996177aa9caee0856e2da2961edb9d709
Gauthier Fiorentino <gauthier.fiorentino@gmail.com>
2021-05-18 16:56:52 +0200

    [STORIES] Added doc to useID stories

--- src/stories/Hooks/useID/useID.stories.tsx
+++ src/stories/Hooks/useID/useID.stories.tsx
@@ -2,8 +2,48 @@ import React, { useState } from 'react'
 import { Story } from '@storybook/react'
 import useID from '../../../lib/hooks/useID/useID'
 
+const documentation = `
+## API
+
+\`\`\`tsx
+const MyComponent = () => {
+  const id = useID()
+  return (
+    <button id={id} />
+  )
+}
+\`\`\`
+
+The ID is unique to the instance, not the render:
+
+\`\`\`tsx
+<>
+  <MyComponent />       /* id: 1 */
+  <MyComponent />       /* id: 2 */
+  <MyOtherComponent />  /* id: 3 */
+</>
+
+// re-render
+
+<>
+  <MyComponent />       /* id: still 1 */
+  <MyComponent />       /* id: still 2 */
+  <MyOtherComponent />  /* id: still 3 */
+</>
+\`\`\`
+
+This is to avoid unnecessary DOM mutations.
+`
+
 export default {
   title: 'Hooks/useID/useID',
+  parameters: {
+    docs: {
+      description: {
+        component: documentation,
+      },
+    },
+  },
 }
 
 const Template: Story = () => {
